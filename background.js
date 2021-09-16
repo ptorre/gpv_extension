905daa440a998893458334eafb6be4df1400638b
Patrick Torre <torre@rotwang.com>
2021-09-16 14:37:23 -0700

    Implement persistent on-off setting

--- background.js
+++ background.js
@@ -1,54 +1,82 @@
 "use strict";
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+let disabled;
 
-async function delayClearBadgeText(ms) {
-  await sleep(ms);
-  chrome.browserAction.setBadgeText({ text: "" });
-}
+chrome.runtime.onMessage.addListener(
+  (message, _sender, sendResponse) => {
+    if (chrome.runtime.lastError) {
+      console.log(`"lastError: ${chrome.runtime.lastError.message}`);
+    } else {
+      if (message === "toggle-enabled") {
+        chrome.storage.local.get("enabled", ({ enabled }) => {
+          if (enabled) {
+            chrome.browserAction.setBadgeText({ text: "OFF" });
+            chrome.storage.local.set({ enabled: false });
+            disabled = true;
+          } else {
+            chrome.browserAction.setBadgeText({ text: "ON" });
+            chrome.storage.local.set({ enabled: true });
+            disabled = false;
+          }
+          sendResponse({ disabled });
+        });
+      }
+    }
+    return true;
+  }
+);
 
 chrome.runtime.onInstalled.addListener(function () {
   console.log("Installed");
-  chrome.browserAction.setBadgeText({ text: "⚡" });
-  delayClearBadgeText(5000)
+  chrome.storage.local.get("enabled", ({enabled}) => {
+    // For each color we were provided…
+    if (typeof enabled === 'undefined') {
+      console.log("Initial install setting is enabled");
+      chrome.storage.local.set({ enabled: true });
+      chrome.browserAction.setBadgeText({ text: "*ON*" });
+      disabled = false;
+    } else if (!enabled) {
+      chrome.browserAction.setBadgeText({ text: "OFF" });
+      disabled = true;
+    } else {
+      chrome.browserAction.setBadgeText({ text: "ON" });
+      disabled = false;
+    }
+  });
 })
-
-chrome.runtime.onSuspend.addListener(function () {
-  console.log("Unloading.");
-  chrome.browserAction.setBadgeText({ text: "" });
-});
 
 // Open requests for video streams in an external player.
 // TODO allow for configuring the player remote address
 // TODO allow for configuring the filter spec externally
 chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
+  function(details) {
+    if (disabled) {
+      return {};
+    }
     var target = "http://localhost:5000/" + details.url;
-    chrome.browserAction.setBadgeText({ text: details.url });
-    console.log("redirecting original request: " + details.url + "to target:" + target)
+    chrome.browserAction.setBadgeText({ text: details.url});
     return { redirectUrl: target };
   },
   {
     urls: [
-      "https://cdnapisec.kaltura.com/*/a.m3u8?*",
-      "https://cdnapisec.kaltura.com/*/a.m3u8",
-      "https://cfvod.kaltura.com/*/index.m3u8?*",
-      "https://cfvod.kaltura.com/*/index.m3u8*",
-      "https://*.cloudfront.net/*/index.webm?*",
-      "https://*.cloudfront.net/*/index.mp4?*"
+      "*://*/*.m3u8*",
+      "*://*/*stream.mpd*",
+      "*://*/*.webm*",
+      "*://*/*.mp4*"
     ],
     types: ["xmlhttprequest", "media"]
   },
   ["blocking"]
 );
 
-// Block Kaltura analytics updates because the  html5 video player sends invalid
+// Block Kaltura analytics updates because the html5 video player sends invalid
 // updates even though it's not playing.
 // TODO allow for configuring the blocking filter spec externally
 chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
+  function (_details) {
+    if (disabled) {
+      return {};
+    }
     return { cancel: true };
   },
   { urls: ["https://analytics.kaltura.com/*"] },
--- popup-url.js
+++ popup-url.js
@@ -1,9 +1,23 @@
 "use strict";
 
+function handleToggle(event) {
+  chrome.runtime.sendMessage("toggle-enabled",
+    function({disabled}) {
+      console.log(`disabled: ${disabled}`)
+      if (disabled) {
+        event.target.classList.replace("enabled", "disabled");
+        event.target.textContent="Enable";
+        textarea.value = "OFF";
+      } else {
+        event.target.classList.replace("disabled", "enabled");
+        event.target.textContent="Disable";
+        textarea.value = "";
+      }
+    }
+  );
+}
+
 let textarea = document.getElementById("url");
-
-chrome.browserAction.getBadgeText({}, function(result) { textarea.value = result })
-
 textarea.onclick = function(element) {
   let url = element.target;
   url.focus();
@@ -11,3 +25,21 @@ textarea.onclick = function(element) {
   url.setSelectionRange(0,99999);
   document.execCommand("copy");
 };
+
+chrome.storage.local.get("enabled", ({enabled}) => {
+  let button = document.createElement("button");
+  if (enabled) {
+    button.classList.add("enabled");
+    button.textContent="Disable";
+    textarea.value = "enabled";
+  } else {
+    button.classList.add("disabled");
+    button.textContent="Enable";
+    textarea.value = "disabled";
+  }
+  button.addEventListener("click", handleToggle);
+
+  let toggle = document.getElementById("toggle");
+  toggle.appendChild(button);
+});
+
